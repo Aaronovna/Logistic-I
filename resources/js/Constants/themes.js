7639b4d216792d924ee8ddc4b4093ab53796665e
Pyromagne <growganoth826@gmail.com>
2024-09-28 21:20:48 +0800

    Prepare to add more themes

--- resources/js/Constants/themes.js
+++ resources/js/Constants/themes.js
@@ -51,23 +51,164 @@ export const gradients = {
     blue_raspberry: 'linear-gradient(to right, #00b4db, #0083b0)',
 }
 
-const feedback={
+const feedback = {
     success: '#8DD7B3',
     warning: '#FFD580',
     danger: '#FF9E8D',
     info: '#80D6E7'
 }
 
-const feedbackVibrant={
+const feedbackVibrant = {
     success: '#36B37E',
     warning: '#FFAB00',
     danger: '#FF5630',
     info: '#00B8D9'
 }
 
-const feedbackLight={
+const feedbackLight = {
     success: '#D4F4E2',  // Very light green for success
     warning: '#FFECB3',  // Very light yellow-orange for warning
-    danger:  '#FFDAD4',  // Very light red-pink for danger
-    info:    '#B3EAF4'   // Very light blue for info
+    danger: '#FFDAD4',  // Very light red-pink for danger
+    info: '#B3EAF4'   // Very light blue for info
 }
+
+export const t1l = {
+    _name: 'ct1',
+    _type: 'light',
+    text: '#061e1b',
+    background: '#f6fefd',
+    primary: '#2fdac9',
+    secondary: '#868de9',
+    accent: '#9167e4',
+    border: '#d1d5db',
+    blur: 'rgba(251, 251, 254, 0.502)',
+    success: '#8DD7B3',
+    warning: '#FFD580',
+    danger: '#FF9E8D',
+    info: '#80D6E7'
+}
+
+export const t1d = {
+    _name: 'ct2',
+    _type: 'dark',
+    text: '#e1f9f6',
+    background: '#010908',
+    primary: '#25d0bf',
+    secondary: '#161d79',
+    accent: '#441b98',
+    border: '#d1d5db',
+    blur: 'rgba(251, 251, 254, 0.502)',
+    success: '#8DD7B3',
+    warning: '#FFD580',
+    danger: '#FF9E8D',
+    info: '#80D6E7'
+}
+
+/* colors: {
+    'text': '#1a0a11',
+    'background': '#f8edf2',
+    'primary': '#662947',
+    'secondary': '#cfbb87',
+    'accent': '#a4b347',
+   },
+
+   colors: {
+    'text': '#f5e5ec',
+    'background': '#12070c',
+    'primary': '#d699b8',
+    'secondary': '#786430',
+    'accent': '#a9b84c',
+   }, */
+
+/*    colors: {
+    'text': '#03191c',
+    'background': '#f6fefe',
+    'primary': '#25d9e9',
+    'secondary': '#7da4f2',
+    'accent': '#4561ed',
+   },
+   colors: {
+    'text': '#e3f9fc',
+    'background': '#010909',
+    'primary': '#16cada',
+    'secondary': '#0d3482',
+    'accent': '#122eba',
+   }, */
+
+/*    colors: {
+    'text': '#202603',
+    'background': '#fefffa',
+    'primary': '#d3e817',
+    'secondary': '#8af4c1',
+    'accent': '#4feede',
+   },
+   colors: {
+    'text': '#f6fcd9',
+    'background': '#040500',
+    'primary': '#d3e817',
+    'secondary': '#0b7542',
+    'accent': '#11b0a0',
+   }, */
+
+/*    colors: {
+    'text': '#0f0c0a',
+    'background': '#f7f5f2',
+    'primary': '#9e8867',
+    'secondary': '#bec9ab',
+    'accent': '#98b389',
+   },
+   colors: {
+    'text': '#f5f2f0',
+    'background': '#0d0b08',
+    'primary': '#988261',
+    'secondary': '#495436',
+    'accent': '#5b764c',
+   }, */
+      
+/*    colors: {
+    'text': '#040911',
+    'background': '#eef3fc',
+    'primary': '#3c7dd3',
+    'secondary': '#d886e4',
+    'accent': '#dd69bc',
+   }, 
+   colors: {
+    'text': '#eef3fb',
+    'background': '#030811',
+    'primary': '#2c6ec3',
+    'secondary': '#6d1b79',
+    'accent': '#962275',
+   }, */
+
+
+/*    colors: {
+    'text': '#0b0804',
+    'background': '#fbf4ef',
+    'primary': '#c6793f',
+    'secondary': '#ccde91',
+    'accent': '#92d369',
+   },
+   colors: {
+    'text': '#fbf8f4',
+    'background': '#100904',
+    'primary': '#c07439',
+    'secondary': '#5c6e21',
+    'accent': '#54962c',
+   },
+    */
+
+/*    colors: {
+    'text': '#13110b',
+    'background': '#fdfdfc',
+    'primary': '#a69c6e',
+    'secondary': '#a3c5c7',
+    'accent': '#848fb3',
+   },
+   colors: {
+    'text': '#f4f2ec',
+    'background': '#030302',
+    'primary': '#918759',
+    'secondary': '#385a5c',
+    'accent': '#4c577b',
+   },
+       */
