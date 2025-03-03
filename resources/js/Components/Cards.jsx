f5ddf23dbeafd00f1466d87375121da5ec00bf78
Pyromagne <growganoth826@gmail.com>
2025-03-03 21:56:16 +0800

    Remove using theme context in card component

--- resources/js/Components/Cards.jsx
+++ resources/js/Components/Cards.jsx
@@ -1,28 +1,24 @@
-import { useStateContext } from "@/context/contextProvider";
-
 import { TbFavicon } from "react-icons/tb";
 import { feedback } from "@/Constants/themes";
 
 export const Card = ({ data, Icon = TbFavicon, iconColor = feedback.info, name = 'Name', className = '' }) => {
-  const { theme, themePreference } = useStateContext();
+  
   return (
-    <div className={`flex rounded-xl py-3 px-4 items-end justify-between w-fit border shadow-md ` + className} style={{ borderColor: theme.border }}>
-      <span className='flex justify-center items-center rounded-md aspect-square w-fit h-fit' style={{ color: theme.background }}>
+    <div className={`flex rounded-xl py-3 px-4 items-end justify-between w-fit border shadow-md text-text` + className}>
+      <span className='flex justify-center items-center rounded-md aspect-square w-fit h-fit'>
         <Icon size={38} title={name} color={iconColor}/>
       </span>
       <span className='ml-4'>
-        <p className={`text-sm ${themePreference === 'light' ? 'text-black/50' : 'text-white/50'}`}>{name}</p>
-        <p className='font-semibold text-xl text-right' style={{ color: theme.text }}>{data ? data : '-'}</p>
+        <p>{name}</p>
+        <p className='font-semibold text-xl text-right'>{data ? data : '-'}</p>
       </span>
     </div>
   )
 }
 
 export const Card2 = ({ data, Icon, iconColor = 'black', name = 'Name', className = '' }) => {
-  const { theme } = useStateContext();
   return (
-    <div className={'rounded-xl pl-4 py-4 border-card shadow-md ' + className}
-      style={{ borderColor: theme.border, color:theme.text }}
+    <div className={'rounded-xl pl-4 py-4 border-card shadow-md text-text' + className}
     >
       <div className="flex flex-col relative min-w-48 h-full">
         <p className='text-sm font-medium text-gray-600 mb-2'>{name}</p>
